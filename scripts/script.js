d06f1a29c1e2e826a316f4e0be32001851103bcf
Oksana Melnyk <oks.melnuk@gmail.com>
2023-11-29 07:12:04 +0100

    added scrollbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,28 @@
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+
 const btnContainer = document.getElementById('btnContainer');
 const rootEl = document.querySelector('.tabs-container');
 
@@ -111,3 +136,74 @@ class ProductList  {
 const list = new ProductList();
 list.btnsHandler();
 list.renderProducts();
+
+
+//Timer
+const deadline = '2023-11-28';
+
+function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    if (t <= 0) {
+        days = 0;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    } else {
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
+        hours = Math.floor((t / (1000 * 60 * 60) % 24));
+        minutes = Math.floor((t / 1000 / 60) % 60);
+        seconds = Math.floor((t / 1000) % 60);
+    }
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    }
+};
+
+function getZero(num) {
+    if(num >= 0 && num < 10) {
+        return `0${num}`
+    } else {
+        return num;
+    }
+}
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds');
+        timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval)
+        }
+    }
+}
+
+setClock('.timer', deadline)
+
+const progress = document.getElementById('progressBar');
+
+const processScroll = () => {
+    let progressWidth = (document.body.scrollTop || document.documentElement.scrollTop)/ (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+    progress.style.width = progressWidth + '%'
+
+}
+document.addEventListener('scroll', processScroll)
+
